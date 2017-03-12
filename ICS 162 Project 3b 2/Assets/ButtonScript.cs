d5e71716bd423a03a79faf03bef5aeacd9cd73b6
Quinn Levine <Qlevine@uci.edu>
2017-03-12 14:33:15 -0700

    Created a base class for buttons so that other scripts can have their own function for when they push buttons.  The torch can be turned off and on now with e.

---
+++ ICS 162 Project 3b 2/Assets/ButtonScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+
+    bool canPress = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public virtual void Update () {
+        if (canPress)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                PressButton();
+            }
+        }
+	}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            canPress = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            canPress = false;
+        }
+    }
+
+
+    public virtual void PressButton()
+    {
+
+    }
+}
---
+++ ICS 162 Project 3b 2/Assets/TorchButton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TorchButton : ButtonScript {
+
+    private GameObject l1;
+    private GameObject l2;
+    private TorchScript t;
+    bool LightOn = true;
+	// Use this for initialization
+	void Start () {
+        t = GetComponent<TorchScript>();
+        l1 = transform.GetChild(0).gameObject;
+        l2 = transform.GetChild(1).gameObject;
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+        base.Update();
+	}
+
+    public override void PressButton()
+    {
+        if (LightOn)
+        {
+            t.enabled = false;
+            l1.SetActive(false);
+            l2.SetActive(false);
+            LightOn = false;
+        }
+        else
+        {
+            t.enabled = true;
+            l1.SetActive(true);
+            l2.SetActive(true);
+            LightOn = true;
+        }
+
+    }
+}
