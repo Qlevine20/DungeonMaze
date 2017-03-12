e3bc40b68dae4d40792eb58f197b2b7488fe32dd
Quinn Levine <Qlevine@uci.edu>
2017-03-11 17:27:34 -0800

    Changed light settings

---
+++ ICS 162 Project 3b 2/Assets/TorchScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TorchScript : MonoBehaviour {
+
+
+    Light l;
+    float minWaitForFlicker = .5f;
+
+    float maxWaitForFlicker = 1;
+    float minIncrementFlicker = .001f;
+    float minAngle = 30;
+    float maxAngle = 32;
+    // Use this for initialization
+    void Start () {
+        l = GetComponentInChildren<Light>();
+        l.type = LightType.Spot;
+        StartCoroutine(flicker());
+	}
+
+    // Update is called once per frame
+
+    
+
+
+    IEnumerator flicker()
+    {
+        l.spotAngle = minAngle;
+        yield return new WaitForSeconds(Random.Range(minWaitForFlicker,maxWaitForFlicker));
+        l.spotAngle = minAngle + 1;
+        yield return new WaitForSeconds(minIncrementFlicker);
+        l.spotAngle = minAngle + 1;
+        yield return new WaitForSeconds(minIncrementFlicker);
+        l.spotAngle = minAngle + 1;
+        yield return new WaitForSeconds(minIncrementFlicker);
+        l.spotAngle = minAngle + 1;
+        yield return new WaitForSeconds(minIncrementFlicker);
+        l.spotAngle = minAngle - 1;
+        yield return new WaitForSeconds(minIncrementFlicker);
+        l.spotAngle = minAngle - 1;
+        yield return new WaitForSeconds(minIncrementFlicker);
+        l.spotAngle = minAngle - 1;
+        yield return new WaitForSeconds(minIncrementFlicker);
+        StartCoroutine(flicker());
+    }
+}
