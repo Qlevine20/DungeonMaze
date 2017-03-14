6006f04a22bc69ecb5fdb39a6cedd6445c6e1d81
Quinn Levine <Qlevine@uci.edu>
2017-03-13 18:13:24 -0700

    Dominos are in and when all are knocked over a bridge falls allowing the player to cross a gap

---
+++ ICS 162 Project 3b 2/Assets/Bridge.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bridge : MonoBehaviour {
+
+    // Use this for initialization
+    public bool fall = false;
+    private Rigidbody rb;
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(fall && !rb.useGravity)
+        {
+            rb.useGravity = true;
+        }
+	}
+
+
+    void OnCollisionEnter(Collision other)
+    {
+        rb.useGravity = false;
+        rb.isKinematic = true;
+    }
+}
---
+++ ICS 162 Project 3b 2/Assets/Domino.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Domino : ButtonScript {
+
+    Animator anim;
+    Bridge b;
+    
+	// Use this for initialization
+	void Start () {
+	    anim = GetComponent<Animator>();
+        b = GameObject.FindGameObjectWithTag("Bridge").GetComponent<Bridge>();
+    }
+	
+	// Update is called once per frame
+	public override void Update () {
+        base.Update();
+	}
+
+    public override void PressButton()
+    {
+        anim.SetBool("PlayAnim", true);
+        StartCoroutine(DominosFall());
+        
+    }
+
+
+    IEnumerator DominosFall()
+    {
+        ThirdPersonCharacter p = GameObject.FindGameObjectWithTag("Player").GetComponent<ThirdPersonCharacter>();
+        Quaternion look = Camera.main.transform.localRotation;
+        Vector3 camLoc = Camera.main.transform.localPosition;
+        Camera.main.transform.parent = transform;
+        Camera.main.transform.localPosition = new Vector3(0, 20, 0);
+        Camera.main.transform.localRotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
+        p.enabled = false;
+        yield return new WaitForSeconds(12);
+        b.fall = true;
+        p.enabled = true;
+        Camera.main.transform.parent = p.transform;
+        Camera.main.transform.localPosition = camLoc;
+        Camera.main.transform.localRotation = look;
+    }
+}
