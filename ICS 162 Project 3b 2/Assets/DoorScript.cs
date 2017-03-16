27e53746bf2e4ffa157ad9e14c9eede93a45914e
Quinn Levine <Qlevine@uci.edu>
2017-03-16 01:17:22 -0700

    The doors and walls are in to make a very basic starting room

---
+++ ICS 162 Project 3b 2/Assets/DoorScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : ButtonScript {
+
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+        base.Update();
+	}
+
+
+    public override void PressButton()
+    {
+        anim.SetBool("Open", true);
+        foreach(BoxCollider b in GetComponents<BoxCollider>())
+        {
+            b.enabled = false;
+        }
+    }
+}
