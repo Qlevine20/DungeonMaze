4a4632f9571b78bccd37e4e4a457892c3ab42817
Quinn Levine <Qlevine@uci.edu>
2017-03-19 12:44:09 -0700

    Added in placeholder objects for the levers and the gates.  The levers and gates have proper scripts as well and they have placeholder animation controllers.  Pressing e next to a button will show the gates and when one opens, you should see it.

---
+++ ICS 162 Project 3b 2/Assets/Maze/Scripts/Gate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gate : MonoBehaviour {
+
+
+    public bool open = false;
+    private bool animPlayed = false;
+    private Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(open && !animPlayed)
+        {
+            if (anim)
+            {
+                anim.SetBool("open", true);
+            }
+        }
+	}
+}
---
+++ ICS 162 Project 3b 2/Assets/Maze/Scripts/LeverScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class LeverScript : ButtonScript {
+
+    public Gate gate;
+    private Animator anim;
+    private Transform camParent;
+    private Vector3 prevCamPos;
+    private Quaternion prevCamRot;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public override void PressButton()
+    {
+        if (gate)
+        {
+            StartCoroutine(OpenGate());
+        }
+    }
+
+
+    IEnumerator OpenGate()
+    {
+        
+        if (anim)
+        {
+            anim.SetBool("Pull", true);
+        }
+        ThirdPersonCharacter p = GameObject.FindGameObjectWithTag("Player").GetComponent<ThirdPersonCharacter>();
+        yield return new WaitForSeconds(1.0f);
+        p.enabled = false;
+        camParent = Camera.main.transform.parent;
+        prevCamPos = Camera.main.transform.localPosition;
+        prevCamRot = Camera.main.transform.localRotation;
+        Camera.main.transform.parent = gate.transform;
+        Camera.main.transform.localPosition = new Vector3(0, 5, 15);
+        Camera.main.transform.localRotation = Quaternion.LookRotation(gate.transform.position - Camera.main.transform.position);
+        gate.open = true;
+        yield return new WaitForSeconds(3);
+        p.enabled = true;
+        Camera.main.transform.parent = camParent;
+        Camera.main.transform.localPosition = prevCamPos;
+        Camera.main.transform.localRotation = prevCamRot;
+
+    }
+}
